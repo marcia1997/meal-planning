520c1a209f864d60ea7964d3ffbb3f1a06a4c413
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2025-02-21 19:12:45 +0200

    Update database.py

--- back-end/database.py
+++ back-end/database.py
@@ -1,9 +1,14 @@
 from pymongo import MongoClient
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
+# MongoDB Connection URL
+MONGO_URI = "mongodb://localhost:27017"
 
-MONGO_URI = os.getenv("MONGO_URI")
+# Create a MongoDB client
 client = MongoClient(MONGO_URI)
-db = client.meal_planner 
+
+# Select the database
+db = client["meal-planning"] 
+
+#  Collections
+users_collection = db["users"]
+recipes_collection = db["recipes"] 
