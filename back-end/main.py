f3e71dcae85d6c4b925994bef968aa97a5c34c17
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2025-04-12 17:05:03 +0300

    Update main.py

--- back-end/main.py
+++ back-end/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
-from app.routes import auth, recipes 
+from app.routes import auth, recipes ,preferences
 
 app = FastAPI()
 
 # Include authentication routes
-app.include_router(auth.router, prefix="/auth")
-app.include_router(recipes.router, prefix="/api")  
+app.include_router(auth.router, prefix="/auth") 
+app.include_router(preferences.router, prefix="/preferences")
+app.include_router(recipes.router)
+
 
 @app.get("/")
 def home():
