9308931d4c5047e9408d03873fe986399bb24bec
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2025-05-12 21:52:52 +0300

    Update main.py

--- back-end/main.py
+++ back-end/main.py
@@ -1,14 +1,26 @@
 from fastapi import FastAPI
-from app.routes import auth, recipes ,preferences
+from fastapi.middleware.cors import CORSMiddleware
+from app.routes import auth, preferences, recipes
 
 app = FastAPI()
 
-# Include authentication routes
-app.include_router(auth.router, prefix="/auth") 
+# Middleware CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Include the auth routes with the "/auth" prefix
+app.include_router(auth.router, prefix="/auth")
+
+# Include other routes
 app.include_router(preferences.router, prefix="/preferences")
-app.include_router(recipes.router)
+app.include_router(recipes.router, prefix="/recipes")
 
-
+# Health check route
 @app.get("/")
 def home():
     return {"message": "Healthy Meal Planner API is running!"}
