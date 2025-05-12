ad4f808739888f74af4692c27e907d806329e328
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2025-05-12 21:52:29 +0300

    Create axiosInstance.js

---
+++ front-end/utils/axiosInstance.js
@@ -0,0 +1,13 @@
+// utils/axiosInstance.js
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'http://localhost:8000',   // <-- point to FastAPI
+  withCredentials: true,              
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+export default instance;
+
